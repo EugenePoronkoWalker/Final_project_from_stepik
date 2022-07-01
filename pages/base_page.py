3c3bdb2fbf12e1057a7cc5535ac33e07d5eaad53
“eugene <eugenwalkerporonko@gmail.com>
2022-07-01 15:44:49 +0300

    added updates for all files

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        except (NoSuchElementException):
+            return False
+        return True
+
     def open(self):
         self.browser.get(self.url)
-
-
